bd0dd0e0e7b601f462d80d6d54c63339edf825e3
JrFernando <fernandojunior20110@gmail.com>
2018-02-10 19:07:43 -0200

    Importei módulos necessários para trabalhar com os formulários

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { ClientComponent } from './client.component';
 import { ClientListComponent } from './client-list/client-list.component';
+import { ClientCreateEditComponent } from './client-create-edit/client-create-edit.component';
 import { ClienteRoutingModule } from './client.routing.module';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
     ClienteRoutingModule
   ],
-  declarations: [ClientComponent, ClientListComponent],
+  declarations: [
+    ClientComponent,
+    ClientListComponent,
+    ClientCreateEditComponent,
+    FormDebugComponent
+  ],
   exports: [ClientComponent]
 })
 export class ClientModule { }

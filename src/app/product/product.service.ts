f7b36131a76c6b75225eb11357265fb5f2b45bdb
JrFernando <fernandojunior20110@gmail.com>
2018-02-23 02:12:13 -0300

    Add mais funções para o service (Dados mock)

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 
 export declare interface Produto {
   codigo: number;
-  nome: string;
-  quantidade: number;
-  preco: number;
+  descricao: string;
+  estoque: number;
+  preco_venda: number;
 }
 
 @Injectable()
@@ -14,16 +14,54 @@ export class ProductService {
 
   constructor() {
     this.produtos = [
-      {codigo: 1, nome: 'Produto 1', quantidade: 10, preco: 3.0},
-      {codigo: 2, nome: 'Produto 2', quantidade: 20, preco: 2.5},
-      {codigo: 3, nome: 'Produto 3', quantidade: 15, preco: 1.0},
-      {codigo: 4, nome: 'Produto 4', quantidade: 5, preco: 10.0},
-      {codigo: 5, nome: 'Produto 5', quantidade: 25, preco: 5}
+      {codigo: 1, descricao: 'Produto 1', estoque: 10, preco_venda: 3.0},
+      {codigo: 2, descricao: 'Produto 2', estoque: 20, preco_venda: 2.5},
+      {codigo: 3, descricao: 'Produto 3', estoque: 15, preco_venda: 1.0},
+      {codigo: 4, descricao: 'Produto 4', estoque: 5, preco_venda: 10.0},
+      {codigo: 5, descricao: 'Produto 5', estoque: 25, preco_venda: 5}
     ];
    }
 
   getAll() {
     return this.produtos;
+  }
+
+  getId(id: number): Produto {
+    for (let i = 0; i < this.produtos.length; i++) {
+      const product = this.produtos[i];
+      if (product.codigo == id) {
+        return product;
+      }
+    }
+  }
+
+  insert(produto: Produto) {
+    this.produtos.push(produto);
+  }
+
+  update(produto: Produto) {
+    for (let i = 0; i < this.produtos.length; i++) {
+      if (this.produtos[i].codigo == produto.codigo) {
+        this.produtos[i] = produto;
+      }
+    }
+  }
+
+
+  remove(codigo: number) {
+    for (let i = 0; i < this.produtos.length; i++) {
+      if (this.produtos[i].codigo == codigo) {
+        this.produtos[i] = this.produtos[this.produtos.length - 1];
+        this.produtos.pop();
+        this.ordena();
+      }
+    }
+  }
+
+  ordena() {
+    this.produtos.sort((c1: Produto, c2: Produto) => {
+      return c1.codigo - c2.codigo;
+    });
   }
 
 }

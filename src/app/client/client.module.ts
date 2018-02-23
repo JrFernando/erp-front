1d6b41f6e71b50d1ed36fce838b2b13b6298f5f0
JrFernando <fernandojunior20110@gmail.com>
2018-02-23 02:10:11 -0300

    Removi form-debug e adicionei div para o painel funcionar com o form

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -7,7 +7,6 @@ import { ClientComponent } from './client.component';
 import { ClientListComponent } from './client-list/client-list.component';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { ClienteRoutingModule } from './client.routing.module';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
 import { InputMaskModule } from './../shared/input-mask/input-mask.module';
 import { ClientService } from './client.service';
 
@@ -25,7 +24,6 @@ import { ClientService } from './client.service';
     ClientComponent,
     ClientListComponent,
     ClientFormComponent,
-    FormDebugComponent
   ],
   exports: [ClientComponent],
   providers: [ClientService]
